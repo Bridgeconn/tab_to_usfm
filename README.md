ba260df5cd665a974a5ed69e4b0e6435da6f7aa3
uday <udayonrails@hotmail.com>
2016-02-29 00:50:05 +0530

    now user can convert more than one file at one time

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
+# Tab to USFM Converter
+
 This script converts Scripture edited in TAB separated formar to basic USFM tags   /id  Book /c Chapter /v verse  . This can be imported into ParaText Software .
 
-# Please follow below steps for convert file
+### Please follow below steps for convert file
 
-**Make file executable with below command from console/ Terminal
+1. First line of file should not be blank.
+2. Two space or tab between verse number and verse.
+3. All verse should be in single line (enter should not be present between sentences
+4. All original file should be inside folder. (folder name can be anything)
+4. File name should be single word (like: 1Corinthians.txt)
+5. Output file will be create inside output_file folder with extension .usfm
 
-$ chmod +x txt_to_tab.rb
 
-**Please check example file with below command:
+###Make file executable with below command from console/ Terminal
 
-$ ./txt_to_tab.rb text-file.txt
+$ chmod +x tab_to_usfm.rb
+
+###Please check example file with below command:
+
+$ ./tab_to_usfm.rb
 
 
-Developed by Uday Kumar  uday.kumar@bridgeconn.com   Bridge Connectivity Solutions
+Developed by Uday Kumar uday.kumar@bridgeconn.com
+[Bridge Connectivity Solutions](http://bridgeconn.com/)
--- tab_to_usfm.rb
+++ tab_to_usfm.rb
@@ -1,32 +1,34 @@
 #!/usr/bin/env ruby
 
-file_name = ARGV[0]
 require 'csv'
+Dir.glob("**/*.txt") do |file|
+  file_name = file
+  ch = []
+  hash = {}
+  c = {}
+  v = {}
+  h = Hash.new { |hash, key| hash[key] = [] }
+  count=0
+  CSV.foreach("#{file_name}", {:col_sep => "\t"}) do |row|
+    h[:"#{row[1]}"] << row[2].to_s+" "+row[3]
+    hash.store(row[0], h)
+    count = count + 1
+  end
 
-ch = []
-hash = {}
-c = {}
-v = {}
-h = Hash.new { |hash, key| hash[key] = [] }
-count=0
-CSV.foreach("#{file_name}", {:col_sep => "\t"}) do |row|
-  h[:"#{row[1]}"] << row[2].to_s+" "+row[3]
-  hash.store(row[0], h)
-  count = count + 1
-end
-file_name = "output_file"
-output_name = "#{File.basename(file_name, '.*')}.usfm"
-output = File.open(output_name, 'w')
+  directory_name = "output_folder"
+  Dir.mkdir(directory_name) unless File.exists?(directory_name)
 
-hash.each do|k, v|
-  output << "\\id #{k}\n"
-  v.each do |k, v|
-    output << "\\c #{k}\n"
+  output_name = "#{directory_name}/#{File.basename(file_name, '.*')}.usfm"
+  output = File.open(output_name, 'w')
+
+  hash.each do|k, v|
+    output << "\\id #{k}\n"
     v.each do |k, v|
-      output << "\\v #{k}\n"
+      output << "\\c #{k}\n"
+      v.each do |k, v|
+        output << "\\v #{k}\n"
+      end
     end
   end
+  output.close
 end
-output.close
-
-
