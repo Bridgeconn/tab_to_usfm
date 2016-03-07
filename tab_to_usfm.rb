1dd160bf95299ef6945ec329e3842f27cfe26b8f
uday <udayonrails@hotmail.com>
2016-03-07 23:10:28 +0530

    issue with file resolve

--- tab_to_usfm.rb
+++ tab_to_usfm.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'csv'
-require 'pry'
+
 file = ARGV[0]
 ch = []
 hash = {}
@@ -9,8 +9,11 @@ c = {}
 v = {}
 h = Hash.new { |hash, key| hash[key] = [] }
 count=0
-CSV.foreach("#{file}", {:col_sep => "\t"}) do |row|
+CSV.foreach("#{file}", {:col_sep => "\t", :quote_char => "^"}) do |row|
   h[:"#{row[1]}"] << row[2].to_s+" "+row[3]
+  if count == 1
+    row
+  end
   hash.store(row[0], h)
   count = count + 1
 end
@@ -18,10 +21,9 @@ end
 output_name = "#{File.basename(file, '.*')}.usfm"
 output = File.open(output_name, 'w')
 
-hash.each do|k, v|
+hash.each do |k, v|
   output << "\\id #{k}\n"
   v.each do |k, v|
-    puts k
     output << "\\c #{k}\n"
     v.each do |k, v|
       output << "\\v #{k}\n"
