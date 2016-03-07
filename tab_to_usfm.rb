2a82cbf5668c6a05a42352089cd48a608f951060
uday <udayonrails@hotmail.com>
2016-03-07 13:40:12 +0530

    tab to usfm single file converter

--- tab_to_usfm.rb
+++ tab_to_usfm.rb
@@ -1,34 +1,31 @@
 #!/usr/bin/env ruby
 
 require 'csv'
-Dir.glob("**/*.txt") do |file|
-  file_name = file
-  ch = []
-  hash = {}
-  c = {}
-  v = {}
-  h = Hash.new { |hash, key| hash[key] = [] }
-  count=0
-  CSV.foreach("#{file_name}", {:col_sep => "\t"}) do |row|
-    h[:"#{row[1]}"] << row[2].to_s+" "+row[3]
-    hash.store(row[0], h)
-    count = count + 1
-  end
+require 'pry'
+file = ARGV[0]
+ch = []
+hash = {}
+c = {}
+v = {}
+h = Hash.new { |hash, key| hash[key] = [] }
+count=0
+CSV.foreach("#{file}", {:col_sep => "\t"}) do |row|
+  h[:"#{row[1]}"] << row[2].to_s+" "+row[3]
+  hash.store(row[0], h)
+  count = count + 1
+end
 
-  directory_name = "output_folder"
-  Dir.mkdir(directory_name) unless File.exists?(directory_name)
+output_name = "#{File.basename(file, '.*')}.usfm"
+output = File.open(output_name, 'w')
 
-  output_name = "#{directory_name}/#{File.basename(file_name, '.*')}.usfm"
-  output = File.open(output_name, 'w')
-
-  hash.each do|k, v|
-    output << "\\id #{k}\n"
+hash.each do|k, v|
+  output << "\\id #{k}\n"
+  v.each do |k, v|
+    puts k
+    output << "\\c #{k}\n"
     v.each do |k, v|
-      output << "\\c #{k}\n"
-      v.each do |k, v|
-        output << "\\v #{k}\n"
-      end
+      output << "\\v #{k}\n"
     end
   end
-  output.close
 end
+output.close
